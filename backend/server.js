0e16bbef56b22268124267180ace722d322a555e
FENO Aïdane Carlie <aidanecarlie@gmail.com>
2024-05-11 01:27:06 +0300

    first commit

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
+const port = process.env.PORT || 7000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("welcome !!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
