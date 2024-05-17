834c39892cfa7f3da667f0e33657acf26df436a8
FENO Aïdane Carlie <aidanecarlie@gmail.com>
2024-05-17 22:34:34 +0300

    update

--- backend/server.js
+++ backend/server.js
@@ -19,13 +19,3 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
 require("dotenv").config();
-const port = process.env.PORT || 7000;
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("welcome !!");
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
-});
